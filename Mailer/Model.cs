e5e0f01179c6e1c384f8bf47e126f7b872007131
Андрей Заярный <zaazaa@yandex.ru>
2021-07-08 20:06:56 +0300

    Add project files.

---
+++ Mailer/Model.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Mailer
+{
+    public class Model:INotifyPropertyChanged
+    {
+      
+        //Add something
+    
+        //Add something2
+
+        //Lesson2
+        static int Counter { get; set; } = 0;
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+
+        #region Comment
+        //public int Value1
+        //{
+        //    get
+        //    {
+        //        return value1;
+        //    }
+        //    set
+        //    {
+        //        System.Diagnostics.Debug.WriteLine("Value1 is changed!");
+        //        if (value1 != value)
+        //        {
+        //            value1 = value;
+        //            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Value1"));
+        //        }
+        //    }
+        //}
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
